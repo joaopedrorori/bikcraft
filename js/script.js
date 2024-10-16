b0aa7cced72e1d2ff10160d5b31db27d2e515abe
joaopedrorori <joaopedrorr.df@exemplo.com>
2024-10-16 18:14:53 -0300

    area de perguntas frequentes dinamica

--- js/script.js
+++ js/script.js
@@ -20,10 +20,26 @@ function ativarProduto(parametro) {
   if (elemento) {
     elemento.checked = true;
   }
-
-  console.log(elemento);
 }
 
 parametros.forEach(ativarProduto);
 
-console.log(parametros);
+//Perguntas frequentes
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
