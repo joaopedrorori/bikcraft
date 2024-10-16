e3daebaf2374e16847e081e3f8072206ed0a7c64
joaopedrorori <joaopedrorr.df@exemplo.com>
2024-10-16 17:34:55 -0300

    Agora quando voce selecionar uma bicicleta ou seguro para comprar, a pagina de orcamento ja vai abrir com o produto selecionado

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Ativar Links do Menu
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -10,3 +11,19 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+//Ativar itens do orcamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+
+  console.log(elemento);
+}
+
+parametros.forEach(ativarProduto);
+
+console.log(parametros);
